23a24fab9f91711dce68e354093a5a298695fc05
Marcel Eschmann <marcel.eschmann@gmail.com>
2015-11-10 14:02:27 +0100

    Complete ou003

--- ou003/src/TheShortestPath.java
+++ ou003/src/TheShortestPath.java
@@ -2,6 +2,20 @@
  * Created by eschmar on 10/11/15.
  */
 public class TheShortestPath {
+    public class Tuple<X, Y> {
+        public final X x;
+        public final Y y;
+        public Tuple(X x, Y y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        @Override
+        public String toString() {
+            return "(" + x + "," + y + ")";
+        }
+    }
+
     /**
      * The method intermediateStations returns a vector of the
      * intermediate stations that are on the shortest path.
@@ -13,8 +27,25 @@ public class TheShortestPath {
      * @return int[]
      */
     public static int[] intermediateStations(double[] a, double[][] b, double[] c) {
-        // TODO
-        return new int[1];
+        int[] result = {0,0};
+
+        // get the closest station in zone 2
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] < a[result[0]]) {
+                result[0] = i;
+            }
+        }
+
+        double temp = b[result[0]][0] + c[0];
+        // get the closest station in zone 3
+        for (int i = 0; i < b[result[0]].length; i++) {
+            temp = b[result[0]][i] + c[i];
+            if (temp < (b[result[0]][result[1]] + c[result[1]])) {
+                result[1] = i;
+            }
+        }
+
+        return result;
     }
 
     /**
@@ -25,7 +56,7 @@ public class TheShortestPath {
      * @return double
      */
     public static double length(double[] a, double[][] b, double[] c) {
-        // TODO
-        return 0;
+        int[] s = intermediateStations(a, b, c);
+        return a[s[0]] + b[s[0]][s[1]] + c[s[1]];
     }
 }
